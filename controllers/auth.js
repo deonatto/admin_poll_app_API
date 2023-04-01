98058dc6b4918d0478786ddc2cf2f8fd27937c9a
Jesus <deonatto@gmail.com>
2023-04-01 23:13:44 +0100

    create register endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,5 +2,24 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
+export const register = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, role } = req.body;
 
-export const register = async (req, res) => {};
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      role,
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- models/User.js
+++ models/User.js
@@ -24,7 +24,12 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
       min: 5,
-    }
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
   { timestamps: true }
 );
