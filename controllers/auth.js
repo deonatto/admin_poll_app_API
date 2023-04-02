48051ac81de0e091c21a7cc8b992b66bb3dbed6c
Jesus <deonatto@gmail.com>
2023-04-02 20:23:14 +0100

    create login endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
+// REGISTER
 export const register = async (req, res) => {
   try {
     const { firstName, lastName, email, password, role } = req.body;
@@ -19,6 +20,29 @@ export const register = async (req, res) => {
 
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//LOGIN
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //use lean to get plain javascript object to be able to delete password before sending obj to FE
+    const user = await User.findOne({ email }).lean();
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+
+    res.status(200).json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
